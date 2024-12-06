50cf7576b4e614fada6f1578e2b674816bf0c8c3
Bouba27 <bodi63218@eleve.isep.fr>
2024-12-06 16:15:14 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,30 +1,15 @@
-
--- Table des utilisateurs
-CREATE TABLE IF NOT EXISTS utilisateurs (
+DROP TABLE IF EXISTS clients;
+CREATE TABLE clients (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    role TEXT DEFAULT 'user'  -- 'admin' or 'user'
-);
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    nom TEXT NOT NULL,
+    prenom TEXT NOT NULL,
+    adresse TEXT NOT NULL
 
--- Table des livres
-CREATE TABLE IF NOT EXISTS livres (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    titre TEXT NOT NULL,
-    auteur TEXT NOT NULL,
-    categorie TEXT,
-    annee_publication INTEGER,
-    quantite INTEGER DEFAULT 1
+    
 );
+CREATE TABLE livre (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL,
+    );
 
--- Table des prêts
-CREATE TABLE IF NOT EXISTS prets (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    utilisateur_id INTEGER NOT NULL,
-    livre_id INTEGER NOT NULL,
-    date_pret DATE DEFAULT (DATE('now')),
-    date_retour DATE,
-    statut TEXT DEFAULT 'en cours',  -- 'en cours' or 'retourne'
-    FOREIGN KEY(utilisateur_id) REFERENCES utilisateurs(id),
-    FOREIGN KEY(livre_id) REFERENCES livres(id)
-);
