40126a1f064eacef41883f9b7b950f56f34ceee7
Bouba27 <bodi63218@eleve.isep.fr>
2024-12-06 16:40:01 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,16 +1,15 @@
--- Table des utilisateurs
-CREATE TABLE IF NOT EXISTS utilisateurs (
+DROP TABLE IF EXISTS clients;
+CREATE TABLE clients (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
-);
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    nom TEXT NOT NULL,
+    prenom TEXT NOT NULL,
+    adresse TEXT NOT NULL
 
--- Table des livres
-CREATE TABLE IF NOT EXISTS livres (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    titre TEXT NOT NULL,
-    auteur TEXT NOT NULL,
-    categorie TEXT,
-    annee_publication INTEGER
+    
 );
+CREATE TABLE livre (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL,
+    );
 
